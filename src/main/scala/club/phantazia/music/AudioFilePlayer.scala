51f76e3e0eeeb28d868bcc9345a4ace4559a7c58
setrar <setrar@valiha.com>
2014-12-03 21:57:28 -0500

    Full FP implementation

--- src/main/scala/club/phantazia/music/AudioFilePlayer.scala
+++ src/main/scala/club/phantazia/music/AudioFilePlayer.scala
@@ -2,56 +2,49 @@ package club.phantazia.music
 
 import java.io.File;
 import java.io.IOException;
- 
-import javax.sound.sampled.{AudioFormat,AudioInputStream,AudioSystem}
-import javax.sound.sampled.{SourceDataLine,DataLine}
- 
+
+import javax.sound.sampled.{ AudioFormat, AudioInputStream, AudioSystem }
+import javax.sound.sampled.{ SourceDataLine, DataLine }
 
 class AudioFilePlayer {
 
   def play(fileName: String) {
-     val fileAudioStream = AudioSystem.getAudioInputStream(new File(fileName))
-     val channels = fileAudioStream.getFormat().getChannels()
-     val sampleRate = fileAudioStream.getFormat().getSampleRate()
-     val sampleSizeInBits = 16
-     val frameSize = channels * 2
-     val frameRate = sampleRate
-     val bigEndian = false
-     val audioOutputFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, sampleRate, sampleSizeInBits, channels, frameSize, frameRate, bigEndian);
-     val dataLineInfo = new DataLine.Info(classOf[SourceDataLine], audioOutputFormat)
-     val audioStream = AudioSystem.getAudioInputStream(audioOutputFormat, fileAudioStream);
-     val audioLine: Option[SourceDataLine] = Some(AudioSystem.getLine(dataLineInfo).asInstanceOf[SourceDataLine])
- 
-     audioLine match {
-       case Some(line) => {
-         line.open(audioOutputFormat)
-	     line.start()
-	     dataStream(audioStream,line)
-	     line.drain()
-	     line.stop()
-       }
-       case None => ???
-     }
-  }
-  
-  // Ugly fix me
-  def dataStream(stream: AudioInputStream,line: SourceDataLine) {
-     var buffer:Array[Byte] = new Array[Byte](65536);
-	 var n = 0
-     while (n != -1 ) {
-        n = stream.read(buffer, 0, buffer.length)
-        line.write(buffer, 0, n)
-     }
+
+    val fileAudioStream = AudioSystem.getAudioInputStream(new File(fileName))
+    val channels = fileAudioStream.getFormat().getChannels()
+    val sampleRate = fileAudioStream.getFormat().getSampleRate()
+    val sampleSizeInBits = 16
+    val frameSize = channels * 2
+    val frameRate = sampleRate
+    val bufferSizeInKbytes = 64
+    val bigEndian = false
+
+    val audioOutputFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, sampleRate, sampleSizeInBits, channels, frameSize, frameRate, bigEndian);
+    val dataLineInfo = new DataLine.Info(classOf[SourceDataLine], audioOutputFormat)
+    val audioStream = AudioSystem.getAudioInputStream(audioOutputFormat, fileAudioStream);
+    val audioLine: Option[SourceDataLine] = Some(AudioSystem.getLine(dataLineInfo).asInstanceOf[SourceDataLine])
+    val buffer = new Array[Byte](bufferSizeInKbytes * 1024)
+
+    audioLine match {
+      case Some(line) => {
+        line.open(audioOutputFormat)
+        line.start()
+        Stream.continually(audioStream.read(buffer)).takeWhile(_ != -1).foreach(line.write(buffer, 0, _))
+        line.drain()
+        line.stop()
+      }
+      case None => ???
+    }
   }
 }
 
 object AudioFilePlayer {
   def main(args: Array[String]): Unit = {
-        import java.io.File
-        val sep = File.separator
+    import java.io.File
+    val sep = File.separator
 
-        val player = new AudioFilePlayer ();
-//        player.play("src" + sep + "test" + sep + "resources" + sep + "WhatIsThisThingCalledLove.mp3")
-        player.play("src" + sep + "test" + sep + "resources" + sep + "500MilesHigh.ogg")
+    val player = new AudioFilePlayer();
+    player.play("src" + sep + "test" + sep + "resources" + sep + "WhatIsThisThingCalledLove.mp3")
+    //        player.play("src" + sep + "test" + sep + "resources" + sep + "500MilesHigh.ogg")
   }
 }
